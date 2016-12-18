a853efa8a953374771769d77b9021202c7aa29a0
guduxingzou <694628003@qq.com>
2016-12-18 12:53:29 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -18,9 +18,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--save_dir', type=str, default='save',
                        help='model directory to store checkpointed models')
-    parser.add_argument('-n', type=int, default=200,
+    parser.add_argument('-n', type=int, default=500,
                        help='number of words to sample')
-    parser.add_argument('--start', default=u'从前',
+    parser.add_argument('--start', default=u'半夏橘皮燥湿化痰，理气行滞茯苓渗湿健脾以杜生痰之源生姜大枣调和营卫以利解表，滋脾行津以润干燥，是为佐药。',
                        help='prime text')
     parser.add_argument('--sample', type=str, default='weighted',
                        help='three choices:argmax,weighted,combined')
@@ -37,13 +37,13 @@ def sample(args):
     # import the trained model
     model = Model(saved_args, True)
     with tf.Session() as sess:
-	# initialize the model
+        # initialize the model
         tf.global_variables_initializer().run()
         saver = tf.train.Saver(tf.global_variables())
         ckpt = tf.train.get_checkpoint_state(args.save_dir)
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess, ckpt.model_checkpoint_path)
-	    # sample the new sequence word by word
+            # sample the new sequence word by word
             literature = model.sample(sess, words, vocab, args.n, args.start, args.sample)
     with codecs.open('result/sequence.txt','a','utf-8') as f:
         f.write(literature+'\n\n')
